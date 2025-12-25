94c32b9f3a60ed1a5c50565eb07c98e6fd305fc8
Ross Taylor-Turner <ross.turner001@googlemail.com>
2025-12-25 20:27:07 +0000

    chore: configure vitest in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
@@ -7,6 +8,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {},
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test/setup.ts'],
+    include: ['test/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/types/**'],
     },
   },
 });
